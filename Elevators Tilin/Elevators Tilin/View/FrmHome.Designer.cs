e05b77fe290473c7b3464f5f82db55e205fa8a01
GerardoHqz <00034720@uca.edu.sv>
2021-12-09 15:11:15 -0600

    :bell: Add View Home Finish

---
+++ Elevators Tilin/Elevators Tilin/View/FrmHome.Designer.cs
@@ -0,0 +1,290 @@
+﻿
+namespace Elevators_Tilin.View
+{
+    partial class FrmHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlMainMenu = new System.Windows.Forms.Panel();
+            this.BtnInventory = new System.Windows.Forms.Button();
+            this.BtnRepair = new System.Windows.Forms.Button();
+            this.BtnMaintenance = new System.Windows.Forms.Button();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.pnlLogo = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnlTitle = new System.Windows.Forms.Panel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.pnlPrincipal = new System.Windows.Forms.Panel();
+            this.pctHome1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pnlMainMenu.SuspendLayout();
+            this.pnlLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pnlTitle.SuspendLayout();
+            this.pnlPrincipal.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctHome1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlMainMenu
+            // 
+            this.pnlMainMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(71)))));
+            this.pnlMainMenu.Controls.Add(this.pictureBox2);
+            this.pnlMainMenu.Controls.Add(this.BtnInventory);
+            this.pnlMainMenu.Controls.Add(this.BtnRepair);
+            this.pnlMainMenu.Controls.Add(this.BtnMaintenance);
+            this.pnlMainMenu.Controls.Add(this.btnRegister);
+            this.pnlMainMenu.Controls.Add(this.pnlLogo);
+            this.pnlMainMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlMainMenu.Location = new System.Drawing.Point(0, 0);
+            this.pnlMainMenu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlMainMenu.Name = "pnlMainMenu";
+            this.pnlMainMenu.Size = new System.Drawing.Size(248, 593);
+            this.pnlMainMenu.TabIndex = 0;
+            // 
+            // BtnInventory
+            // 
+            this.BtnInventory.BackColor = System.Drawing.Color.Transparent;
+            this.BtnInventory.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BtnInventory.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BtnInventory.FlatAppearance.BorderSize = 0;
+            this.BtnInventory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnInventory.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnInventory.ForeColor = System.Drawing.Color.White;
+            this.BtnInventory.Image = global::Elevators_Tilin.Properties.Resources.IcoInventario__1_;
+            this.BtnInventory.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.BtnInventory.Location = new System.Drawing.Point(0, 268);
+            this.BtnInventory.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.BtnInventory.Name = "BtnInventory";
+            this.BtnInventory.Size = new System.Drawing.Size(248, 62);
+            this.BtnInventory.TabIndex = 6;
+            this.BtnInventory.Text = "Inventario";
+            this.BtnInventory.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnInventory.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BtnInventory.UseVisualStyleBackColor = false;
+            this.BtnInventory.Click += new System.EventHandler(this.BtnInventory_Click);
+            // 
+            // BtnRepair
+            // 
+            this.BtnRepair.BackColor = System.Drawing.Color.Transparent;
+            this.BtnRepair.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BtnRepair.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BtnRepair.FlatAppearance.BorderSize = 0;
+            this.BtnRepair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnRepair.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnRepair.ForeColor = System.Drawing.Color.White;
+            this.BtnRepair.Image = global::Elevators_Tilin.Properties.Resources.IcoRepairpng__1_;
+            this.BtnRepair.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.BtnRepair.Location = new System.Drawing.Point(0, 206);
+            this.BtnRepair.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.BtnRepair.Name = "BtnRepair";
+            this.BtnRepair.Size = new System.Drawing.Size(248, 62);
+            this.BtnRepair.TabIndex = 5;
+            this.BtnRepair.Text = "Reparaciones";
+            this.BtnRepair.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnRepair.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BtnRepair.UseVisualStyleBackColor = false;
+            this.BtnRepair.Click += new System.EventHandler(this.BtnRepair_Click);
+            // 
+            // BtnMaintenance
+            // 
+            this.BtnMaintenance.BackColor = System.Drawing.Color.Transparent;
+            this.BtnMaintenance.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BtnMaintenance.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BtnMaintenance.FlatAppearance.BorderSize = 0;
+            this.BtnMaintenance.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnMaintenance.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnMaintenance.ForeColor = System.Drawing.Color.White;
+            this.BtnMaintenance.Image = global::Elevators_Tilin.Properties.Resources.IcoMantenimiento__1_;
+            this.BtnMaintenance.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.BtnMaintenance.Location = new System.Drawing.Point(0, 144);
+            this.BtnMaintenance.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.BtnMaintenance.Name = "BtnMaintenance";
+            this.BtnMaintenance.Size = new System.Drawing.Size(248, 62);
+            this.BtnMaintenance.TabIndex = 4;
+            this.BtnMaintenance.Text = "Mantenimientos     ";
+            this.BtnMaintenance.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnMaintenance.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BtnMaintenance.UseVisualStyleBackColor = false;
+            this.BtnMaintenance.Click += new System.EventHandler(this.BtnMaintenance_Click);
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.BackColor = System.Drawing.Color.Transparent;
+            this.btnRegister.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnRegister.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnRegister.FlatAppearance.BorderSize = 0;
+            this.btnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegister.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnRegister.ForeColor = System.Drawing.Color.White;
+            this.btnRegister.Image = global::Elevators_Tilin.Properties.Resources.registrar;
+            this.btnRegister.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnRegister.Location = new System.Drawing.Point(0, 82);
+            this.btnRegister.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(248, 62);
+            this.btnRegister.TabIndex = 3;
+            this.btnRegister.Text = "Registrar      ";
+            this.btnRegister.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRegister.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnRegister.UseVisualStyleBackColor = false;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // pnlLogo
+            // 
+            this.pnlLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.pnlLogo.Controls.Add(this.pictureBox1);
+            this.pnlLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlLogo.Location = new System.Drawing.Point(0, 0);
+            this.pnlLogo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlLogo.Name = "pnlLogo";
+            this.pnlLogo.Size = new System.Drawing.Size(248, 82);
+            this.pnlLogo.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Elevators_Tilin.Properties.Resources.Logo_SIAL;
+            this.pictureBox1.Location = new System.Drawing.Point(22, 5);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(209, 73);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pnlTitle
+            // 
+            this.pnlTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
+            this.pnlTitle.Controls.Add(this.btnClose);
+            this.pnlTitle.Controls.Add(this.lblTitle);
+            this.pnlTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTitle.Location = new System.Drawing.Point(248, 0);
+            this.pnlTitle.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlTitle.Name = "pnlTitle";
+            this.pnlTitle.Size = new System.Drawing.Size(816, 82);
+            this.pnlTitle.TabIndex = 1;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackgroundImage = global::Elevators_Tilin.Properties.Resources.close_grey;
+            this.btnClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Location = new System.Drawing.Point(29, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(60, 54);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTitle.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblTitle.Location = new System.Drawing.Point(350, 12);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(143, 54);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "HOME";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnlPrincipal
+            // 
+            this.pnlPrincipal.Controls.Add(this.pctHome1);
+            this.pnlPrincipal.Location = new System.Drawing.Point(248, 80);
+            this.pnlPrincipal.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlPrincipal.Name = "pnlPrincipal";
+            this.pnlPrincipal.Size = new System.Drawing.Size(816, 513);
+            this.pnlPrincipal.TabIndex = 2;
+            // 
+            // pctHome1
+            // 
+            this.pctHome1.Image = global::Elevators_Tilin.Properties.Resources.Fondo;
+            this.pctHome1.Location = new System.Drawing.Point(0, -8);
+            this.pctHome1.Name = "pctHome1";
+            this.pctHome1.Size = new System.Drawing.Size(816, 521);
+            this.pctHome1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctHome1.TabIndex = 0;
+            this.pctHome1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Elevators_Tilin.Properties.Resources.logo_tilin;
+            this.pictureBox2.Location = new System.Drawing.Point(22, 537);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(185, 45);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // FrmHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1064, 593);
+            this.Controls.Add(this.pnlPrincipal);
+            this.Controls.Add(this.pnlTitle);
+            this.Controls.Add(this.pnlMainMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.Name = "FrmHome";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SIAL : Menú Principal";
+            this.pnlMainMenu.ResumeLayout(false);
+            this.pnlLogo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnlTitle.ResumeLayout(false);
+            this.pnlTitle.PerformLayout();
+            this.pnlPrincipal.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pctHome1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlMainMenu;
+        private System.Windows.Forms.Panel pnlLogo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel pnlTitle;
+        private System.Windows.Forms.Panel pnlPrincipal;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button BtnInventory;
+        private System.Windows.Forms.Button BtnRepair;
+        private System.Windows.Forms.Button BtnMaintenance;
+        private System.Windows.Forms.PictureBox pctHome1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
